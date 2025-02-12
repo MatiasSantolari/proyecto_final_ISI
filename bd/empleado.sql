994efbdde89136f06125db4e1d73415569671d13
Facundo B <facubasualdo49@gmail.com>
2025-02-12 20:51:19 -0300

    Agregada tabla empleado

---
+++ bd/empleado.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS `empleado`;
+CREATE TABLE `empleado` (
+  `id_empleado` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `estado` ENUM('inactivo', 'en licencia', 'suspendido', 'en prueba', 'jubilado') NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
