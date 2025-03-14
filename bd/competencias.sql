57cff96733d631730c1c7c91d2ff9f082d91dd91
Facundo B <facubasualdo49@gmail.com>
2025-03-14 14:52:45 -0300

    Cambiado nombre a archivos sql y agregado historial_asistencia que se me habia pasado

--- bd/competencias.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `competencias`;
-CREATE TABLE `competencias` (
-  `id_competencia` INT NOT NULL AUTO_INCREMENT,
-  `descripcion` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id_competencia`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- bd/departamento.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `departamento`;
-CREATE TABLE `departamento` (
-  `id_departamento` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `nombre` VARCHAR(100) NOT NULL,
-  `descripcion` VARCHAR(255)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
