7708ff04f2ae50ed304e66585f997a9a0ac60be1
Facundo B <facubasualdo49@gmail.com>
2025-09-18 03:46:53 -0300

    Cambios y agregados en Models para Evaluacion, Criterios, TiposCriterios

--- core/models/evaluacion_empleado.py
+++ core/models/evaluacion_empleado.py
@@ -6,6 +6,7 @@ from .evaluacion import Evaluacion
 class EvaluacionEmpleado(models.Model):
     evaluacion = models.ForeignKey(Evaluacion, on_delete=models.CASCADE)
     empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    evaluador = models.ForeignKey(Empleado, on_delete=models.SET_NULL, null=True, blank=True)
     fecha_registro = models.DateField(verbose_name='Fecha de registro')
     comentarios = models.CharField(max_length=255, null=True, blank=True, verbose_name='Comentarios de la evaluacion')
     calificacion_final = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Calificacion final de la evaluacion')
